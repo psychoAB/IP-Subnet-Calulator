5d695f69a175e4e1db34b19a925a17acc65d0349
psychoAB <dome.2499@hotmail.com>
2017-12-08 02:12:08 +0700

    add all possible network

--- src/main.js
+++ src/main.js
@@ -125,6 +125,62 @@ function getArpa(payload) {
     return ipToString(ipAddr) + '.in-addr.arpa'
 }
 
+function getIPList(payload) {
+    var ipList = []
+    var networkList = []
+    var rangeList = []
+    var i = 0
+
+    networkList.push(payload['hostNumMasked'])
+
+    while(networkList.length < payload['broadcastIPList'].length) {
+        networkList.push(payload['broadcastIPList'][i])
+    }
+
+    for(i = 0; i < payload['broadcastIPList'].length; i++) {
+        payload['broadcastIPList'][i] = payload['broadcastIPList'][i] - 1
+
+        rangeList[i] = numToString(networkList[i] + 1) + ' - ' + numToString(payload['broadcastIPList'][i] - 1)
+    }
+
+    for(i = 0; i < payload['broadcastIPList'].length; i++) {
+        var temp = {}
+        
+        temp['network'] = networkList[i]
+        temp['range'] = rangeList[i]
+        temp['broadcast'] = payload['broadcastIPList'][i]
+
+        ipList.push(temp)
+    }
+
+    return ipList
+} 
+function count(payload) {
+    var subnet = []
+    var runner = payload['hostNumMasked']
+
+    while(runner <= payload['ipEnd']) {
+        runner = runner + (0x80000000 >>> (payload['subnetBitNum'] - 1))
+        subnet.push(runner)
+    }
+
+    return subnet
+}
+
+function getIPEnd(payload) {
+    if(payload['ipClass'] == 'A') {
+        return payload['hostNumMasked'] | 0x00FFFFFF
+    }
+    else if(payload['ipClass'] == 'B') {
+        return payload['hostNumMasked'] | 0x0000FFFF
+    }
+    else if(payload['ipClass'] == 'C') {
+        return payload['hostNumMasked'] | 0x000000FF
+    }
+
+    return byteMask
+}
+
 function getIPType(payload) {
     for(ipRange in privateIP) {
         var start = stringToNum(privateIP[ipRange][0])
@@ -187,6 +243,42 @@ function getBroadcastNum(payload) {
     var broadcastNum = networkNum | ((~payload['subnetNum']) & byteMask)
 
     return broadcastNum
+}
+
+function getMockupIP(payload) {
+    s = numToString(payload['hostNum'])
+    var delimiters = []
+
+    for(c in s) {
+        if(s.charAt(c) == '.') {
+            delimiters.push(parseInt(c))
+        }
+    }
+
+    if(payload['ipClass'] == 'A') {
+        return s.substring(0, delimiters[0]) + '.*.*.*'
+    }
+    else if(payload['ipClass'] == 'B') {
+        return s.substring(0, delimiters[1]) + '.*.*'
+    }
+    else if(payload['ipClass'] == 'C') {
+        return s.substring(0, delimiters[2]) + '.*'
+    }
+
+    return '*.*.*.*'
+}
+
+function getSubnetMaskOfNetworkClass(payload) {
+    if(payload['ipClass'] == 'A') {
+        return 0xFF000000
+    }
+    else if(payload['ipClass'] == 'B') {
+        return 0xFFFF0000
+    }
+    else if(payload['ipClass'] == 'C') {
+        return 0xFFFFFF00
+    }
+    return 0x0
 }
 
 function getNetworkClassRadio() {
@@ -253,6 +345,7 @@ function refreshSubnet() {
 
 function render(payload) {
     var infoTable = document.getElementById('infoTable')
+    var ipTable = document.getElementById('ipTable')
 
     while(infoTable.hasChildNodes()) {
         infoTable.removeChild(infoTable.lastChild)
@@ -312,6 +405,64 @@ function render(payload) {
         row.appendChild(data)
 
         infoTable.appendChild(row)
+    }
+    
+    var ipTablePara = document.getElementById('ipTablePara')
+    var ipTableTitle = document.createElement('h3')
+    var ipTable = document.getElementById('ipTable')
+    var ipList
+    var mockupIP
+
+    while(ipTablePara.hasChildNodes()) {
+        ipTablePara.removeChild(ipTablePara.lastChild)
+    }
+
+    while(ipTable.hasChildNodes()) {
+        ipTable.removeChild(ipTable.lastChild)
+    }
+
+    payload['ipClassSubnetMask'] = getSubnetMaskOfNetworkClass(payload)
+    payload['hostNumMasked'] = payload['hostNum'] & payload['ipClassSubnetMask']
+    payload['ipEnd'] =  getIPEnd(payload)
+    payload['broadcastIPList'] = count(payload)
+    
+    ipList = getIPList(payload)
+    mockupIP = getMockupIP(payload)
+
+    ipTableTitle.innerHTML = 'All Possible ' + payload['cidr'] + ' Networks for ' + mockupIP
+
+    ipTablePara.appendChild(ipTableTitle)
+
+    var row = document.createElement('tr')
+    var networkAddr = document.createElement('td')
+    var usableHostRange = document.createElement('td')
+    var broadcastAddr = document.createElement('td')
+
+    networkAddr.innerHTML = 'Network Address'
+    usableHostRange.innerHTML = 'Usable Host Range'
+    broadcastAddr.innerHTML = 'Broadcast Address'
+
+    row.appendChild(networkAddr)
+    row.appendChild(usableHostRange)
+    row.appendChild(broadcastAddr)
+
+    ipTable.appendChild(row)
+
+    for(element in ipList) {
+        var row = document.createElement('tr')
+        var networkAddr = document.createElement('td')
+        var usableHostRange = document.createElement('td')
+        var broadcastAddr = document.createElement('td')
+
+        networkAddr.innerHTML = numToString(ipList[element]['network'])
+        usableHostRange.innerHTML = ipList[element]['range']
+        broadcastAddr.innerHTML = numToString(ipList[element]['broadcast'])
+
+        row.appendChild(networkAddr)
+        row.appendChild(usableHostRange)
+        row.appendChild(broadcastAddr)
+
+        ipTable.appendChild(row)
     }
 }
 
